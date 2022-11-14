edfa85e087525bdf21b41e8e1526980cdbe8da0b
gvirus21 <gouravkumar21.dev@gmail.com>
2022-11-14 20:44:35 +0530

    WIP - partially completed header & herosection

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,23 +2,31 @@ import React from "react";
 
 const style = {
   container:
-    "px-28 py-12 flex bg-transparent justify-between transition-all ease-in-out delay-150 absolute top-0 left-0 right-0",
-  logo: "text-xl text-white",
-  linksContainer: "flex",
+    "px-28 sm:px-14 py-12 sm:py-20 flex bg-transparent justify-between items-center transition-all ease-in-out delay-150 absolute top-0 left-0 right-0",
+  logo: "text-8xl md:text-2xl lg:text-xl text-white",
+  linksContainer: "hidden lg:flex",
   link: "px-4 text-white cursor-pointer transition-all ease-in-out delay-250 hover:text-gray-500",
+  menu: "bg-gray-700 h-20 w-24 md:block lg:hidden",
 };
 
 const Header = () => {
   return (
-      <div className={style.container}>
-        <p className={style.logo}>Gourav kumar</p>
-        <ul className={style.linksContainer}>
-          <li className={style.link}>About</li>
-          <li className={style.link}>Work</li>
-          <li className={style.link}>Projects</li>
-          <li className={style.link}>Contact</li>
-        </ul>
+    <div className={style.container}>
+      <p className={style.logo}>Gourav kumar</p>
+
+      {/* links */}
+      <ul className={style.linksContainer}>
+        <li className={style.link}>About</li>
+        <li className={style.link}>Work</li>
+        <li className={style.link}>Projects</li>
+        <li className={style.link}>Contact</li>
+      </ul>
+
+      {/* hamburger */}
+      <div className={style.menu}>
+
       </div>
+    </div>
   );
 };
 
--- app/Herosection.tsx
+++ app/Herosection.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Herosection = () => {
   return (
-    <div className='flex flex-col justify-center w-screen h-screen bg-black px-28'>
-        <h2 className='text-white text-[2rem] ml-4'>Hi, i am</h2>
-        <h2 className='text-white text-[7rem]'>Gourav Kumar</h2>
-        <h2 className='text-white text-[2rem] mt-5 ml-4'>Web3 frontend developer</h2>
-    </div>
-  )
-}
+    <div className="flex flex-col justify-center w-screen h-screen px-28 ">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute left-0 w-full h-full object-cover -z-10"
+      >
+        {/* this video is temporary */}
+        <source src="/assets/background.mp4" type="video/mp4" />
+      </video>
 
-export default Herosection
+      <div className="z-10">
+        <h2 className="text-white text-[2rem] ml-4">Hi, i am</h2>
+        <h2 className="text-white text-[7rem]">Gourav Kumar</h2>
+        <h2 className="text-white text-[2rem] mt-5 ml-4">
+          Web3 frontend developer
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Herosection;
