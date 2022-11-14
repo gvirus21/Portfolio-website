225870da0af5f6bb9168e6c931b23022f4383ec4
gvirus21 <gouravkumar21.dev@gmail.com>
2022-11-14 13:27:46 +0530

    Re-arranged stylings for header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import {style} from '../styles/HeaderStyle'
+import React from "react";
 
+const style = {
+  container:
+    "px-28 py-12 flex bg-transparent justify-between transition-all ease-in-out delay-150 absolute top-0 left-0 right-0",
+  logo: "text-xl text-white",
+  linksContainer: "flex",
+  link: "px-4 text-white cursor-pointer transition-all ease-in-out delay-250 hover:text-gray-500",
+};
 
 const Header = () => {
-
   return (
-    <div className={style.container}> 
+      <div className={style.container}>
         <p className={style.logo}>Gourav kumar</p>
         <ul className={style.linksContainer}>
-            <li className={style.link}>About</li>
-            <li className={style.link}>Work</li>
-            <li className={style.link}>Projects</li>
-            <li className={style.link}>Contact</li>
+          <li className={style.link}>About</li>
+          <li className={style.link}>Work</li>
+          <li className={style.link}>Projects</li>
+          <li className={style.link}>Contact</li>
         </ul>
-    </div>
-  )
-}
+      </div>
+  );
+};
 
-export default Header
+export default Header;
--- styles/HeaderStyle.ts
+++
@@ -1,6 +0,0 @@
-export const style = {
-    container: "px-28 py-8 flex bg-transparent justify-between transition-all ease-in-out delay-150",
-    logo: "text-xl",
-    linksContainer: "flex",
-    link: "px-4 cursor-pointer transition-all ease-in-out delay-250 hover:text-white"
-}
