62c9a8c1fa0aef48b0940215672089f2510be07d
gvirus21 <gouravkumar21.dev@gmail.com>
2022-11-15 14:45:19 +0530

    Added ham menu and improved logo responsiveness

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 import React, { useContext } from "react";
 import { CursorContext } from "../context/CursorContext";
 
 const style = {
   container:
     "px-28 sm:px-14 py-12 sm:py-20 flex bg-transparent justify-between items-center transition-all ease-in-out delay-150 absolute top-0 left-0 right-0 mix-blend-difference",
-  logo: "text-8xl md:text-2xl lg:text-xl text-white",
+  logo: "text-5xl text-white",
   linksContainer: "hidden lg:flex",
   link: "px-4 text-white transition-all ease-in-out delay-250",
   menu: "bg-gray-700 h-20 w-24 md:block lg:hidden",
@@ -57,7 +57,14 @@ const Header = () => {
       </ul>
 
       {/* hamburger */}
-      <div className={style.menu}></div>
+      <div
+        onMouseEnter={cursorContext?.textEnter}
+        onMouseLeave={cursorContext?.textLeave}
+        className="flex flex-col justify-between items-center h-10 w-24 lg:hidden"
+      >
+        <div className="w-20 h-1 bg-white rounded"></div>
+        <div className="w-20 h-1 bg-white rounded"></div>
+      </div>
     </div>
   );
 };
