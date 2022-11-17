3ebef17377e9b97dc3645e4a0fad9e52572b87f1
gvirus21 <gouravkumar21.dev@gmail.com>
2022-11-17 12:54:29 +0530

    Added links to the social links and improved code readability

--- components/Header/MobileExpandedMenu.tsx
+++ components/Header/MobileExpandedMenu.tsx
@@ -1,30 +1,45 @@
 import React from "react";
+import Link from "next/link";
 import { motion } from "framer-motion";
 import { linkVariant, containerVariant } from "./variants";
+import {
+  AiOutlineTwitter,
+  AiOutlineGithub,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { IconContext } from "react-icons/lib";
 
 const style = {
   link: "text-white text-9xl my-8",
-  socialLinks: "text-white text-8xl my-8",
+  socialLinks: "text-white text-7xl my-5 flex items-end",
 };
 
 const MobileExpandedMenu = () => {
-  const MenuLinks = ["About", "Work", "Projects", "Contact"];
-  const socialLinks = ["Twitter", "GitHub", "LinkedIn"];
+  const menuLinks = ["About", "Work", "Projects", "Contact"];
 
-  return (
-    <motion.div
-      variants={containerVariant}
-      initial="initial"
-      animate="visible"
-      exit={{
-        opacity: 0,
-        transition: { duration: 0.5, delay: 0.5, ease: "easeInOut" },
-      }}
-      className="bg-black h-screen w-screen absolute z-20 pl-20 flex flex-col justify-between"
-    >
+  const socialLinks = [
+    {
+      name: "Twitter",
+      link: "https://www.twitter.com/GouravKumarDev",
+      icon: <AiOutlineTwitter />,
+    },
+    {
+      name: "Github",
+      link: "https://www.github.com/gvirus21",
+      icon: <AiOutlineGithub />,
+    },
+    {
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/gouravkumar-21/",
+      icon: <AiFillLinkedin />,
+    },
+  ];
+
+  const MenuLinks = () => {
+    return (
       <div className="mt-60 justify-between">
         <ul>
-          {MenuLinks.map((link, index) => {
+          {menuLinks.map((link, index) => {
             return (
               <motion.li
                 key={index}
@@ -53,37 +68,65 @@ const MobileExpandedMenu = () => {
           })}
         </ul>
       </div>
+    );
+  };
+
+  const SocialLinks = () => {
+    return (
       <div className="mb-24">
         <ul>
-          {socialLinks.map((link, index) => {
+          {socialLinks.map((item, index) => {
             return (
-              <motion.li
-                key={index}
-                exit={{
-                  opacity: 0,
-                  y: -50,
-                  transition: {
-                    duration: 0.2,
+            <a target="_blank" rel="noopener noreferrer" href="/">
+              // <a target="_blank" href={item.link} rel="noopener noreferrer">
+                <motion.li
+                  key={index}
+                  exit={{
+                    opacity: 0,
+                    y: -50,
+                    transition: {
+                      duration: 0.2,
+                      delay: index * 0.1,
+                      ease: "easeInOut",
+                    },
+                  }}
+                  variants={linkVariant}
+                  initial="initial"
+                  animate="visible"
+                  transition={{
+                    duration: 0.4,
                     delay: index * 0.1,
                     ease: "easeInOut",
-                  },
-                }}
-                variants={linkVariant}
-                initial="initial"
-                animate="visible"
-                transition={{
-                  duration: 0.4,
-                  delay: index * 0.1,
-                  ease: "easeInOut",
-                }}
-                className={style.socialLinks}
-              >
-                {link}
-              </motion.li>
+                  }}
+                  className={style.socialLinks}
+                >
+                  {item.name}
+                  <div className="ml-5">
+                    <IconContext.Provider value={{size: '65'}}>{item.icon}</IconContext.Provider>
+                  </div>
+                </motion.li>
+              // </a>
+            </a>
             );
           })}
         </ul>
       </div>
+    );
+  };
+
+  return (
+    <motion.div
+      variants={containerVariant}
+      initial="initial"
+      animate="visible"
+      exit={{
+        opacity: 0,
+        transition: { duration: 0.5, delay: 0.5, ease: "easeInOut" },
+      }}
+      className="bg-black h-screen w-screen absolute z-20 pl-20 flex flex-col justify-between"
+    >
+      <MenuLinks />
+      <SocialLinks />
     </motion.div>
   );
 };
