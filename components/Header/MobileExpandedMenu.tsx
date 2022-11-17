e62f5fd55adba30154b850eced5fb42d8e925f2e
gvirus21 <gouravkumar21.dev@gmail.com>
2022-11-17 11:37:39 +0530

    Improved exit animations of the mobile menu

--- components/Header/MobileExpandedMenu.tsx
+++ components/Header/MobileExpandedMenu.tsx
@@ -16,7 +16,10 @@ const MobileExpandedMenu = () => {
       variants={containerVariant}
       initial="initial"
       animate="visible"
-      exit={{ opacity: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
+      exit={{
+        opacity: 0,
+        transition: { duration: 0.5, delay: 0.5, ease: "easeInOut" },
+      }}
       className="bg-black h-screen w-screen absolute z-20 pl-20 flex flex-col justify-between"
     >
       <div className="mt-60 justify-between">
@@ -25,6 +28,15 @@ const MobileExpandedMenu = () => {
             return (
               <motion.li
                 key={index}
+                exit={{
+                  opacity: 0,
+                  y: -50,
+                  transition: {
+                    duration: 0.2,
+                    delay: index * 0.1,
+                    ease: "easeInOut",
+                  },
+                }}
                 variants={linkVariant}
                 initial="initial"
                 animate="visible"
@@ -47,6 +59,15 @@ const MobileExpandedMenu = () => {
             return (
               <motion.li
                 key={index}
+                exit={{
+                  opacity: 0,
+                  y: -50,
+                  transition: {
+                    duration: 0.2,
+                    delay: index * 0.1,
+                    ease: "easeInOut",
+                  },
+                }}
                 variants={linkVariant}
                 initial="initial"
                 animate="visible"
