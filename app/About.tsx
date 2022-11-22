9cb3e5d5097721ed14c8a030ad0d1fa246b534b2
gvirus21 <gouravkumar21.dev@gmail.com>
2022-11-22 13:10:43 +0530

    WIP - working on animation

--- app/About.tsx
+++ app/About.tsx
@@ -1,10 +1,22 @@
 "use client";
 import Image from "next/image";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { CursorContext } from "../context/CursorContext";
+import { motion } from "framer-motion";
+
+const HeadingVariant = {
+  initial: { y: 60, opacity: 0 },
+  visible: { y: 0, opacity: 1, transition: { ease: "easeInOut", duration: 0.5 } },
+};
+
+const ImageVariant = {
+  initial: { y: 60, opacity: 0 },
+  visible: { y: 0, opacity: 1, transition: { ease: "easeInOut", duration: 0.5, delay: 0.3 } },
+};
 
 const About = () => {
   const cursorContext = useContext(CursorContext);
+
 
   return (
     <div
@@ -12,20 +24,26 @@ const About = () => {
       onMouseLeave={cursorContext?.textLeave}
       className="grid place-items-center h-screen w-screen z-50 bg-white xs:pt-24 md:pt-0"
     >
-      <div className="h-full w-full md:max-w-[80rem] md:max-h-[35rem]">
+      <div
+        className="h-full w-full md:max-w-[80rem] md:max-h-[35rem]"
+      >
         <div className="w-full flex xs:justify-center lg:justify-start lg:pl-20">
-          <h1
+          <motion.h1
+            variants={HeadingVariant}
+            initial="initial"
+            whileInView='visible'
+            viewport={{once: false, amount: 0.3}}
             onMouseEnter={cursorContext?.blackBackgroundEnter}
             onMouseLeave={cursorContext?.whiteBackgroundEnter}
             className="text-9xl ml-10"
             className="xs:text-7xl md:text-9xl xs:text-center inline-block lg:text-left"
           >
             About
-          </h1>
+          </motion.h1>
         </div>
-        <div className="xs:w-full md:w-6/6 h-full mx-auto mt-8 flex justify-start md:justify-center xs:items-center md:items-start xs:flex-col md:flex-row">
 
-          <div className="xs:h-[24rem] md:h-[35rem] xs:w-[20rem] md:w-[28rem]  relative ">
+        <div className="xs:w-full md:w-6/6 h-full mx-auto mt-8 flex justify-start md:justify-center xs:items-center md:items-start xs:flex-col md:flex-row">
+          <motion.div variants={ImageVariant} initial="initial" whileInView='visible' viewport={{once: false, amount: 0.4}} className="xs:h-[24rem] md:h-[35rem] xs:w-[20rem] md:w-[28rem] relative ">
             {/* border */}
             <div className="h-[20rem] w-[20rem] border-4 border-black rounded-lg absolute xs:top-10 xs:right-0 md:top-16 md:right-14" />
             {/* profile image */}
@@ -38,7 +56,7 @@ const About = () => {
                 className="rounded-lg"
               />
             </div>
-          </div>
+          </motion.div>
 
           <div className="xs:w-5/6 md:w-3/6 flex lg:justify-center md:justify-between lg:ml-10 ">
             <ul className="xs:w-full md:w-5/6 xs:max-h-[30rem]">
