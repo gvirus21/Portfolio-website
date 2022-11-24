e0249b64c0d788956c8b9c25f9fc871835f69f09
gvirus21 <gouravkumar21.dev@gmail.com>
2022-11-24 14:32:50 +0530

    Added work-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Cursor from "../components/utils/Cursor";
-import Header from "./Header"
-import Herosection from "./Herosection"
-import About from "./About"
-import Projects from "./Projects"
+import Header from "./Header";
+import Herosection from "./Herosection";
+import About from "./About";
+import Work from "./Work";
+import Projects from "./Projects";
 const page = () => {
   return (
     <div className="relative">
@@ -11,6 +12,7 @@ const page = () => {
       <Header />
       <Herosection />
       <About />
+      <Work />
       <Projects />
     </div>
   );
