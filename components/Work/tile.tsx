8fe37acb2838c64e2171e6e18edffd9e7ff3268c
gvirus21 <gouravkumar21.dev@gmail.com>
2022-11-27 13:09:38 +0530

    WIP - Added Sticky parallax scrolling tiles to work page

---
+++ components/Work/tile.tsx
@@ -0,0 +1,101 @@
+import React, { useRef, useContext } from "react";
+import ScrollObserver, { ScrollContext } from "../../context/ScrollObserver";
+
+interface WrapperProps {
+  children: React.ReactNode;
+  numOfPages: number;
+}
+
+interface TileContentProps {
+  children: React.ReactNode;
+}
+
+interface TileContextValue {
+  numOfPages: number;
+  currentPage: number;
+}
+
+const TileContext = React.createContext<TileContextValue>({
+  numOfPages: 0,
+  currentPage: 0,
+});
+
+export const TileWrapper: React.FC<WrapperProps> = ({
+  children,
+  numOfPages,
+}) => {
+  const { scrollY } = useContext(ScrollContext);
+  const refContainer = useRef<HTMLDivElement>(null);
+
+  let currentPage = 0;
+  const { current: elContainer } = refContainer;
+
+  if (elContainer) {
+    const { clientHeight, offsetTop } = elContainer;
+    const screenH = window.innerHeight;
+    const halfH = screenH / 2;
+    const percentY =
+      Math.min(
+        clientHeight + halfH,
+        Math.max(-screenH, scrollY - offsetTop) + halfH
+      ) / clientHeight;
+
+    currentPage = percentY * numOfPages;
+  }
+
+  return (
+    <TileContext.Provider
+      value={{
+        numOfPages,
+        currentPage,
+      }}
+    >
+      <div
+        ref={refContainer}
+        className="realtive bg-white"
+        style={{ height: numOfPages * 100 + "vh" }}
+      >
+        {children}
+      </div>
+    </TileContext.Provider>
+  );
+};
+
+export const TileBackground: React.FC = () => (
+  <div className="absolute h-screen w-screen" />
+);
+
+export const TileContent: React.FC<TileContentProps> = ({ children }) => (
+  <div className="sticky top-0 h-screen z-0">
+    <div className='sticky h-screen z-50'>{children}</div>
+   </div>
+);
+
+interface Props {
+  page: number;
+  renderContent: (props: { progress: number }) => any;
+}
+
+export const Tile: React.FC<Props> = ({ page, renderContent }) => {
+  const { currentPage, numOfPages } = useContext(TileContext);
+  const progress = Math.max(0, currentPage - page);
+  const refContainer = useRef<HTMLDivElement>(null);
+
+  let opacity = Math.min(1, Math.max(0, progress * 4));
+  if (progress > 0.85 && page < numOfPages - 1) {
+    opacity = Math.max(0, (1.0 - progress) * 4);
+  }
+
+  return (
+    <div
+      ref={refContainer}
+      className="absolute top-0 w-full"
+      style={{
+        pointerEvents: progress >= 0 || progress >= 1 ? "none" : undefined,
+        opacity,
+      }}
+    >
+      {renderContent({ progress })}
+    </div>
+  );
+};
